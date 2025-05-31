31f00ad80841c4cb2e869460edb1aa1d95de8fc3
Eduardo Q <qe.eduardo99@gmail.com>
2025-05-30 21:10:03 -0600

    feat: organize files and return track info

--- create_dataset/spotify_auth.py
+++ create_dataset/spotify_auth.py
@@ -8,8 +8,6 @@ from flask import Flask, session, url_for, request, redirect
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.urandom(64)
 
-##SETTING UP AUTHENTICATION
-
 load_dotenv(".env")
 CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
 CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
@@ -25,16 +23,29 @@ sp_oauth = SpotifyOAuth(
     cache_handler=CACHE_HANDLER,
     show_dialog=True,
 )
-
-##CREATE INSTANCE OF SPOTIFY CLIENT
 sp = Spotify(auth_manager=sp_oauth)
-##ENDPOINTS
 
 
 def validate_token():
     if not sp_oauth.validate_token(CACHE_HANDLER.get_cached_token()):
         auth_url = sp_oauth.get_authorize_url()
         return redirect(auth_url)
+
+
+def get_track_info(track):
+    return {
+        "id": track["track"]["id"],
+        "name": track["track"]["name"],
+        "added_at": track["added_at"],
+        "album_id": track["track"]["album"]["id"],
+        "album_name": track["track"]["album"]["name"],
+        "album_imgs": track["track"]["album"]["images"],
+        "artist_id": track["track"]["artists"][0]["id"],
+        "artists_name": track["track"]["artists"][0]["name"],
+        "duration_ms": track["track"]["duration_ms"],
+        "is_explicit": track["track"]["explicit"],
+        "popularity": track["track"]["popularity"],
+    }
 
 
 @app.route("/")
@@ -54,9 +65,9 @@ def get_playlists():
     validate_token()
     playlists = sp.current_user_playlists()
     playlist_id = playlists["items"][0]["id"]
-    print("Playlist ID:", playlist_id)
     playlist_tracks = sp.playlist_tracks(playlist_id)
-    return {"playlist": playlists["items"][0], "tracks": playlist_tracks}
+    playlist_tracks = [get_track_info(item) for item in playlist_tracks["items"]]
+    return playlist_tracks
 
 
 @app.route("/logout")
